358132ba715b32aa82e83ab2b92fac73d8600410
Emmanuel Pastor <ommanu13@hotmail.fr>
2026-07-14 11:03:27 +0200

    Add kotlinx-datetime dependency
    Signed-off-by: Skyincap Admin <tools-admin@skyincap.com>

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -38,6 +38,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.datetime)
         }
 
         commonTest.dependencies {
